6b1b7fea54e0b4ab7f8c7ed00ca0dc5cca242e81
sohelsarder <sohel@mpower-social.com>
2018-03-27 14:11:49 +0600

    python demo project

---
+++ print.py
@@ -0,0 +1,13 @@
+import sys
+print(sys.argv)
+
+name = "adAr alI"
+
+
+print("=======python print demo ==========")
+
+print(name.title())
+print(name.upper())
+print(name.lower())
+print(name.lower().title())
+
